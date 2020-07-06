bf1636f7e386075b31ceeec28397b29a50e27968
veyselboybay <veysel.boybay@outlook.com>
2020-07-06 19:21:59 -0400

    first commit

--- first.py
+++ first.py
@@ -1,4 +1,4 @@
-a,b=5,10;
+a,b,c=5,5,None
 
 
 
@@ -7,3 +7,17 @@ print("Hello , world!");
 
 print(a);
 print("hello",a);
+
+if (a<b):
+    print("A is less than b")
+elif(a>b):
+    print("A is greater than B")
+else:
+    print("A is equal to b")
+
+if(a==b):
+    print("a is equal to b")
+
+
+for i in range(1,6):
+    print(i)
